aa2b3bc7f3bb5b4a5b09e420a35d9231e0923580
xiexuyuyan <2351783158@qq.com>
2022-10-31 20:34:51 +0800

    0.0.12 Core board configure using USB

--- stm32f401ccu6-core-board/board/board.c
+++ stm32f401ccu6-core-board/board/board.c
@@ -34,7 +34,7 @@ void SystemClock_Config(void)
     RCC_OscInitStruct.PLL.PLLM = 15;
     RCC_OscInitStruct.PLL.PLLN = 144;
     RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
-    RCC_OscInitStruct.PLL.PLLQ = 4;
+    RCC_OscInitStruct.PLL.PLLQ = 5;
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
     {
         Error_Handler();
@@ -52,5 +52,63 @@ void SystemClock_Config(void)
     if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
     {
         Error_Handler();
+    }
+}
+
+void HAL_PCD_MspInit(PCD_HandleTypeDef* pcdHandle)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    if(pcdHandle->Instance==USB_OTG_FS)
+    {
+        /* USER CODE BEGIN USB_OTG_FS_MspInit 0 */
+
+        /* USER CODE END USB_OTG_FS_MspInit 0 */
+
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        /**USB_OTG_FS GPIO Configuration
+        PA11     ------> USB_OTG_FS_DM
+        PA12     ------> USB_OTG_FS_DP
+        */
+        GPIO_InitStruct.Pin = GPIO_PIN_11|GPIO_PIN_12;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+        GPIO_InitStruct.Alternate = GPIO_AF10_OTG_FS;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+        /* Peripheral clock enable */
+        __HAL_RCC_USB_OTG_FS_CLK_ENABLE();
+
+        /* Peripheral interrupt init */
+        HAL_NVIC_SetPriority(OTG_FS_IRQn, 0, 0);
+        HAL_NVIC_EnableIRQ(OTG_FS_IRQn);
+        /* USER CODE BEGIN USB_OTG_FS_MspInit 1 */
+
+        /* USER CODE END USB_OTG_FS_MspInit 1 */
+    }
+}
+
+void HAL_PCD_MspDeInit(PCD_HandleTypeDef* pcdHandle)
+{
+    if(pcdHandle->Instance==USB_OTG_FS)
+    {
+        /* USER CODE BEGIN USB_OTG_FS_MspDeInit 0 */
+
+        /* USER CODE END USB_OTG_FS_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_USB_OTG_FS_CLK_DISABLE();
+
+        /**USB_OTG_FS GPIO Configuration
+        PA11     ------> USB_OTG_FS_DM
+        PA12     ------> USB_OTG_FS_DP
+        */
+        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11|GPIO_PIN_12);
+
+        /* Peripheral interrupt Deinit*/
+        HAL_NVIC_DisableIRQ(OTG_FS_IRQn);
+
+        /* USER CODE BEGIN USB_OTG_FS_MspDeInit 1 */
+
+        /* USER CODE END USB_OTG_FS_MspDeInit 1 */
     }
 }
