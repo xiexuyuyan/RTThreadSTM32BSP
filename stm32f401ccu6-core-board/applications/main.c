df44eccf00426a2fe29d6891c0e37c930ba43134
Coder Wu <2351783158@qq.com>
2022-11-25 00:17:47 +0800

    0.0.19 在main添加小功能，5秒后切换finsh和console到vcom设备 修复Kconfig uart2和usb不冲突，默认启用

--- stm32f401ccu6-core-board/applications/main.c
+++ stm32f401ccu6-core-board/applications/main.c
@@ -39,9 +39,35 @@ int main(void)
             , keyEventCallback, (void*)RT_NULL);
     rt_pin_irq_enable(KEY0_PIN, PIN_IRQ_ENABLE);
 
+    rt_device_t vcom = rt_device_find("vcom");
+    char buff[15];
+    if (rt_device_open(
+            vcom, RT_DEVICE_OFLAG_RDWR
+            | RT_DEVICE_FLAG_INT_RX
+            | RT_DEVICE_FLAG_STREAM) != RT_EOK) {
+        rt_pin_write(LED0_PIN, PIN_LOW);
+        return -1;
+    }
+
+    char* const waiting = "waiting...";
+    char* s = waiting;
+    char* d = buff;
+    buff[14] = '\0';
+    while (*d++ = *s++);
+
     while (count++)
     {
         rt_thread_mdelay(1000);
+
+        if (count < 4) {
+            rt_device_write(vcom, 0, (rt_sprintf(buff, "%s%d%c", waiting, count, '\n'), buff), strlen(buff));
+        } else if (count == 4) {
+            rt_device_write(vcom, 0, (rt_sprintf(buff, "%s%c", "last second", '\n'), buff), strlen(buff));
+            rt_device_close(vcom);
+        } else if (count == 5) {
+            rt_console_set_device("vcom");
+            finsh_set_device("vcom");
+        }
     }
 
     return RT_EOK;
